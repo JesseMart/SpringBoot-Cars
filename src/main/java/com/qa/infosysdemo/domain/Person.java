90ab74defba2062bff5009aca7cd39599cd2f0d1
Jesse Martinez <jessemartinez452@gmail.com>
2021-06-10 11:57:18 -0500

    added repo and entity

--- src/main/java/com/qa/infosysdemo/domain/Person.java
+++ src/main/java/com/qa/infosysdemo/domain/Person.java
@@ -1,12 +1,23 @@
 package com.qa.infosysdemo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //-- Entity annotation is used to represent this class as a table in the DB
 public class Person {
 
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	
 	private String name;
 	private int age;
 	
-	public Person() {};
+	public Person() {}; // Default Constructor is requiered with NO arguments
+	
 	public Person(String name, int age) {
 		super();
 		this.name = name;
